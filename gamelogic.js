b1f2e96239a7f4e09f155c9237fce50745f7e377
O <oliosborne@gmail.com>
2021-06-06 19:40:25 +0100

    Added javascript file. card selection methods added.

---
+++ gamelogic.js
@@ -0,0 +1,11 @@
+function choose_card() {
+
+}
+
+function highlight_card(){
+    document.getElementById("pl_hand_pos_1").style.borderColor = "red";
+}
+
+function unhighlight_card(){
+    document.getElementById("pl_hand_pos_1").style.borderColor = "black";
+}
