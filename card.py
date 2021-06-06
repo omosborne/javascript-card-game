f4c28671a18c4271ba10e81122f4384095e50c11
O <oliosborne@gmail.com>
2021-06-06 13:21:22 +0100

    Added card statistics.

--- card.py
+++ card.py
@@ -3,9 +3,14 @@
 
 class Card:
     def __init__(self, card_name):
-        # self.card_names = ["Card 1", "Card 2", "Card 3", "Card 4"]
         self.name = card_name
-        # random.choice(self.card_names)
+        self.image = None
+        self.stats = {
+            "North": None,
+            "East": None,
+            "South": None,
+            "West": None,
+        }
 
     def get_name(self):
         return self.name
