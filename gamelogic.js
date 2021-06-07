0c4f886d306c827c493693124e484ce6351d5ca5
O <oliosborne@gmail.com>
2021-06-07 16:02:35 +0100

    Attach card div to mouse without deleting the original div.

--- gamelogic.js
+++ gamelogic.js
@@ -1,7 +1,8 @@
 let is_droppable = false;
 let chosen_position = null;
+let card_collected = false;
 
-function choose_card(selected_pos) {
+/*function choose_card(selected_pos) {
     //let selected_card = document.getElementsByClassName(child_class)
     //selected_card.style.visibility = "hidden";
     // selected_pos.closest(".card").remove();
@@ -9,29 +10,58 @@ function choose_card(selected_pos) {
     //selected_pos.removeChild(child_class);
     //document.getElementById("pl_hand_pos_1").innerHTML = selected_pos.firstElementChild.innerHTML;
     //const myNode = document.getElementById("foo");
-    selected_pos.innerHTML = '';
 
-    const move_card_div = document.createElement("div");
-    move_card_div.className = "card";
-    move_card_div.id = "move_card"
+    //selected_pos.innerHTML = '';
 
-    const move_card_img = document.createElement("img");
-    move_card_img.setAttribute('src', 'example_card.png');
-    move_card_img.setAttribute('alt', 'example');
-    move_card_img.setAttribute('width', '100');
-    move_card_img.setAttribute('ondragstart', 'return false;');
-
-    move_card_div.appendChild(move_card_img);
+    // const move_card_div = document.createElement("div");
+    // move_card_div.className = "card";
+    // move_card_div.id = "move_card"
+    //
+    // const move_card_img = document.createElement("img");
+    // move_card_img.setAttribute('src', 'example_card.png');
+    // move_card_img.setAttribute('alt', 'example');
+    // move_card_img.setAttribute('width', '100');
+    // move_card_img.setAttribute('ondragstart', 'return false;');
+    //
+    // move_card_div.appendChild(move_card_img);
 
     //const currentDiv = document.getElementById(selected_pos);
     //document.body.insertBefore(move_card_div, selected_pos);
     //selected_pos.appendChild(move_card_div);
-    document.body.appendChild(move_card_div);
 
+    //document.body.appendChild(move_card_div);
+
+    let chosen_card = selected_pos;
+    chosen_card.style.position = "absolute";
+    //document.body.appendChild(chosen_card());
 
     const onMouseMove = (e) =>{
-        move_card_div.style.left = e.pageX + 'px';
-        move_card_div.style.top = e.pageY + 'px';
+        //chosen_card.style.left = e.pageX + 'px';
+        //chosen_card.style.top = e.pageY + 'px';
+
+        let shiftX = e.clientX - chosen_card.getBoundingClientRect().left;
+        let shiftY = e.clientY - chosen_card.getBoundingClientRect().top;
+
+        chosen_card.style.position = 'absolute';
+        chosen_card.style.zIndex = "1000";
+        document.body.append(chosen_card);
+
+        moveAt(e.pageX, e.pageY);
+
+        function moveAt(pageX, pageY) {
+        chosen_card.style.left = pageX - shiftX + 'px';
+        chosen_card.style.top = pageY - shiftY + 'px';
+        }
+
+        function onMouseMove(e){
+            moveAt(e.pageX, e.pageY);
+            chosen_card.hidden = true;
+            let elemBelow = document.elementFromPoint(e.clientX, e.clientY);
+            chosen_card.hidden = false;
+
+            //if (!elemBelow) return;
+        }
+
     }
 
     document.addEventListener('mousemove', onMouseMove);
@@ -58,10 +88,20 @@ function choose_card(selected_pos) {
     }
 
     document.addEventListener('mouseup', onMouseUp);
+}*/
 
+function choose_card(selected_pos){
 
-
+    let chosen_card = selected_pos.firstElementChild;
+    if (!card_collected) {
+        card_collected = true
+        document.addEventListener('mousemove', function(ev){
+            chosen_card.style.transform = 'translateY('+(ev.clientY-180)+'px)';
+            chosen_card.style.transform += 'translateX('+(ev.clientX-50)+'px)';
+        },false);
+    }
 }
+
 
 function droppable(mouseOver, chosen_pos){
     if (chosen_pos.querySelectorAll(".card").length > 0) {
