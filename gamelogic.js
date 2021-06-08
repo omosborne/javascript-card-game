1411b606eb733076d91eded54839b69847ab4726
GetFlawed <cairokv62@gmail.com>
2021-06-08 16:56:16 +0100

    Changed select to card rather than position.

--- gamelogic.js
+++ gamelogic.js
@@ -3,15 +3,18 @@ let chosen_card = null;
 
 function choose_card(selected_pos){
     [ 'pl_hand_pos_1', 'pl_hand_pos_2', 'pl_hand_pos_3', 'pl_hand_pos_4', 'pl_hand_pos_5' ].forEach(function( hand_pos ) {
-            document.getElementById(hand_pos).style.transform = "translateX(0px)";
-            document.getElementById(hand_pos).style.removeProperty("border");
+            if (document.getElementById(hand_pos).querySelectorAll(".card").length > 0) {
+                document.getElementById(hand_pos).firstElementChild.style.transform = "translateX(0px)";
+                document.getElementById(hand_pos).firstElementChild.style.removeProperty("border");
+            }
     });
+
     if (selected_pos.querySelectorAll(".card").length > 0) {
         if (!(chosen_card === selected_pos.firstElementChild)){
             card_chosen = true;
             chosen_card = selected_pos.firstElementChild;
-            selected_pos.style.transform = "translateX(20px)";
-            selected_pos.style.border = "solid red";
+            chosen_card.style.transform = "translateX(20px)";
+            chosen_card.style.border = "solid red";
         }
         else {
             card_chosen = false;
@@ -22,8 +25,8 @@ function choose_card(selected_pos){
 
 function summon(selected_pos){
     if (!(selected_pos.querySelectorAll(".card").length > 0)) {
-        chosen_card.parentElement.style.removeProperty("border");
-        chosen_card.parentElement.style.transform = "translateX(0px)";
+        chosen_card.style.removeProperty("border");
+        chosen_card.style.transform = "translateX(0px)";
         chosen_card.parentElement.style.zIndex = "-1";
         card_chosen = false;
         selected_pos.appendChild(chosen_card);
@@ -32,13 +35,13 @@ function summon(selected_pos){
 }
 
 function highlight_card(selected_pos){
-    selected_pos.style.transform = "translateX(20px)";
+    selected_pos.firstElementChild.style.transform = "translateX(20px)";
 }
 
 function unhighlight_card(selected_pos){
     if (chosen_card === null || !(chosen_card.parentNode === selected_pos))
     {
-        selected_pos.style.transform = "translateX(0px)";
+        selected_pos.firstElementChild.style.transform = "translateX(0px)";
 
     }
 }
