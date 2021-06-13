35141c9ce67a0f9d85190e46c523849d9eb5c383
GetFlawed <cairokv62@gmail.com>
2021-06-13 13:51:15 +0100

    Added loop for when screen loads, hand positions are scaled to fit the player area height.

--- gamelogic.js
+++ gamelogic.js
@@ -5,27 +5,33 @@ let player_cards = ['card_tier_3.png', 'card_tier_1.png', 'card_tier_1.png', 'ca
 function screen_size(){
     document.getElementById("game_area").style.height = ((window.innerHeight) * 0.8).toString();
     document.getElementById("player_area").style.height = ((window.innerHeight) * 0.2).toString();
-    let scale = 0.01;
-    let x = (((window.innerWidth - (window.innerWidth * .5)) * .5));
-    let y = (((window.innerHeight - (window.innerHeight * .2)) * .5));
-    document.getElementById("grid").style.transformOrigin = x + "px " + y + "px";
-    x -= 55;
-    y -= 77.5;
-    document.getElementById("grid").style.transform = "scale(" + scale +") translate(" + x +"px, " + y + "px)";
+    let grid_scale = 0.01;
+    let grid_x = (((window.innerWidth - (window.innerWidth * .5)) * .5));
+    let grid_y = (((window.innerHeight - (window.innerHeight * .2)) * .5));
+    document.getElementById("grid").style.transformOrigin = grid_x + "px " + grid_y + "px";
+    grid_x -= 55;
+    grid_y -= 77.5;
+    document.getElementById("grid").style.transform = "scale(" + grid_scale +") translate(" + grid_x +"px, " + grid_y + "px)";
     let gap = document.getElementById("position_2").getBoundingClientRect().top.toString();
     if (parseInt(gap) > 30){
         while (parseInt(gap) > 30){
-            scale = scale + 0.01;
-            document.getElementById("grid").style.transform = "scale(" + scale +") translate(" + x +"px, " + y + "px)";
+            grid_scale = grid_scale + 0.01;
+            document.getElementById("grid").style.transform = "scale(" + grid_scale +") translate(" + grid_x +"px, " + grid_y + "px)";
             gap = document.getElementById("position_2").getBoundingClientRect().top.toString();
         }
     }
-    document.getElementById("pl_hand_pos_1").style.width = document.getElementById("position_5").style.width;
-    document.getElementById("pl_hand_pos_2").style.width = document.getElementById("position_5").style.width;
-    document.getElementById("pl_hand_pos_3").style.width = document.getElementById("position_5").style.width;
-    document.getElementById("pl_hand_pos_4").style.width = document.getElementById("position_5").style.width;
-    document.getElementById("pl_hand_pos_5").style.width = document.getElementById("position_5").style.width;
 
+    let pl_hand = document.getElementById("player_hand");
+    for (let i = 0; i < pl_hand.children.length; i++) {
+        let card_scale = 0.01;
+        pl_hand.children[i].style.transform = "scale(" + card_scale +")";
+        if (pl_hand.children[i].getBoundingClientRect().height < document.getElementById("player_area").getBoundingClientRect().height){
+            while (pl_hand.children[i].getBoundingClientRect().height < document.getElementById("player_area").getBoundingClientRect().height){
+                card_scale = card_scale + 0.01;
+                pl_hand.children[i].style.transform = "scale(" + card_scale +")";
+            }
+        }
+    }
 }
 window.addEventListener('resize', screen_size);
 
