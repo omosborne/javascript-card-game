436856384e8e986b6d71ac723e1c611690489698
GetFlawed <cairokv62@gmail.com>
2021-06-14 17:34:53 +0100

    Added the display for the discard pile

--- gamelogic.js
+++ gamelogic.js
@@ -63,7 +63,9 @@ function screen_size(){
     resize_player_hand();
 
     document.getElementById("pl_deck_count").style.transform = "translateX(" + (((document.getElementById("player_deck").getBoundingClientRect().width) * .5) - 4)+ "px)";
+    document.getElementById("pl_discard_count").style.transform = "translateX(" + ((((document.getElementById("player_deck").getBoundingClientRect().width) * .5) * 3)- 4) + "px)";
     document.getElementById("pl_deck_count").style.visibility = "visible";
+    document.getElementById("pl_discard_count").style.visibility = "visible";
 }
 
 //creating a placeholder value so that cards can be resized to this (perfect scaling based on browser)
@@ -101,6 +103,7 @@ function show(){
 
 function pl_deck_count(){
     document.getElementById("pl_deck_count").innerHTML = document.getElementById("player_deck").children.length.toString();
+    document.getElementById("pl_discard_count").innerHTML = document.getElementById("discard_pile").children.length.toString();
 }
 //Loads the cards in the hand
 function load_hand(){
