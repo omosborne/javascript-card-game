719c4b48543afb92bbd6b2fda5967e2c3f68f8a5
O <oliosborne@gmail.com>
2021-06-19 12:59:06 +0100

    Fixed #5, removed Y-axis rotation that revered card images.

--- gamelogic.js
+++ gamelogic.js
@@ -501,6 +501,7 @@ function inspect_card(card) {
     }
     else {
         card_inspector.style.backgroundImage = inspected_card.style.backgroundImage;
+        card_inspector.children[0].style.transform = "rotateY(0deg)";
         card_inspector.children[0].children[0].style.backgroundImage = inspected_card.children[1].children[0].style.backgroundImage;
         card_inspector.children[0].children[1].style.backgroundImage = inspected_card.children[1].children[1].style.backgroundImage;
         card_inspector.children[0].children[2].style.backgroundImage = inspected_card.children[1].children[2].style.backgroundImage;
